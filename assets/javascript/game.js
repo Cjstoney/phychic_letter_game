0822a7d097a852ce9b573d688a3f429f10028d60
Camerone Stoney <Cameronestoney@yahoo.com>
2019-04-24 14:30:02 -0700

    javascript started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,22 @@
 // generate random letter
+function randLetter(){
+    var letterList= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var letter = letterList[Math.floor(Math.random()* letterList.length)];
+     console.log(letter)
+}
 
-// capture user keystroke
+randLetter();
+// capture user keystrokecd ../
+document.onkeyup = function(event){
+    var userGuess= event.key;
+}
 
 // compare user keystroke to computer generated lettter
 
 // if the letter matches, alert that the user wins and add one to the win column
-
+if (randLetter === userGuess){
+    alert("User Wins")
+}
 // else add the user selected letter to the letters guessed section and take awayone chance
 
 // if all the chances are used, alert that computer wins and reset the game
