3a9010517345e11bf9711020b3304b20ba44d0f9
Camerone Stoney <Cameronestoney@yahoo.com>
2019-04-24 13:56:49 -0700

    html skeleton and js psuedocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// generate random letter
+
+// capture user keystroke
+
+// compare user keystroke to computer generated lettter
+
+// if the letter matches, alert that the user wins and add one to the win column
+
+// else add the user selected letter to the letters guessed section and take awayone chance
+
+// if all the chances are used, alert that computer wins and reset the game
